57157b20a1cfe40581de5cdc6e2d58ce52f86e5f
Jacob Hoggen <jacob.hoggen@medieinstitutet.se>
2021-11-01 12:18:30 +0100

    added-stored-items-to-cartpage

--- cart.js
+++ cart.js
@@ -1,56 +1,84 @@
 
 
-function getProductsFromCart() {
 
-    let cart = localStorage.getItem("cart")
+function getProductsFromCart() {
+    
+    let cart = localStorage.getItem("cart")         //här skulle det inte va med , product
 
     if(cart) {
-        cart = Json.parse(cart)
+        cart = JSON.parse(cart)
     } else {
         cart = []
     }
     
     let main = document.getElementsByTagName("main")[0];
-
+    console.log(cart)
     for (var i = 0; i <cart.length; i++) {
-        let itemContainer = createCartContainer(cart[i])
+        let itemContainer = createCartContainer(cart[i]);
+        
         main.appendChild(itemContainer)
     };
-
-
 }
 
+
 function createCartContainer(cartItem) {
+    // product card container
     let itemContainer = document.createElement("div")
     itemContainer.classList.add("itemContainer")
-
+    
     // image container 
     let cartImgContainer = document.createElement("div")
-    cartImgContainer.classLisr.add("cartImgContainer")
+    cartImgContainer.classList.add("cartImgContainer")
 
     let itemImg = document.createElement("img")
     itemImg.classList.add("itemImg")
     itemImg.src =  "./assets/" + cartItem.product.image
 
     cartImgContainer.append(itemImg)
-
-
+    
     // Title container 
+    let itemTitleContainer = document.createElement("div")
+    itemTitleContainer.classList.add("itemTitleContainer")
+
     let itemTitle = document.createElement("h1")
     itemTitle.classList.add("itemTitle")
-    itemTitle.innerText = cart.title
+    itemTitle.innerText = cartItem.product.title
+
+    itemTitleContainer.append(itemTitle)
 
     // Price Container 
+    let itemPriceContainer = document.createElement("div")
+    itemPriceContainer.classList.add("itemPriceContainer")
+
     let itemPrice = document.createElement("h2")
     itemPrice.classList.add("itemPrice")
-    itemPrice.innerText = cart.price + " kr"
+    itemPrice.innerText = cartItem.product.price + " kr"
 
-    itemTitle.append(itemPrice)
+    itemPriceContainer.append(itemPrice)
 
-    itemContainer.append(itemImg, itemTitle, itemPrice)
-    return itemContainer 
+    //Button Container
+    let removeItemButtonContainer = document.createElement("div")
+    removeItemButtonContainer.classList.add("removeItemButtonContainer")
+    removeItemButtonContainer.addEventListener("click", () => {
+        removeItemFromCart(cartItem)
+    });
+
+    let icon = document.createElement("i")
+    icon.className = "far fa-trash-alt"
+    icon.classList.add("trashcanCart")
+
+    let buttonRemoveItemTextContainer = document.createElement("div")
+    buttonRemoveItemTextContainer.innerText = "Ta bort"
+
+    removeItemButtonContainer.append(icon, buttonRemoveItemTextContainer)
+
+    itemContainer.append(cartImgContainer, itemTitleContainer, itemPriceContainer, removeItemButtonContainer)
+    return itemContainer
 }
 
+function removeItemFromCart(cartItem) {
+    localStorage.removeItem(cartItem)
+}
 
 
 function getNrOfCartItems() {
@@ -71,10 +99,10 @@ function getNrOfCartItems() {
     })
 
     saveProducts.innerText = amount 
-    console.log(cart)
 }
 
 getNrOfCartItems()
+getProductsFromCart()           //kallade på funktionen
 
 
 
